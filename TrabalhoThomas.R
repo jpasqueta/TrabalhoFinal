48798ea221d88ac3a2c5f5bc1976cded065754a0
jpasqueta <127438025+jpasqueta@users.noreply.github.com>
2023-06-15 09:13:22 -0300

    Add files via upload

---
+++ TrabalhoThomas.R
@@ -0,0 +1,3 @@
+## Trabalho Final Jéssica e Mica
+## Câncer de colo de útero, dados de comportamento de risco
+## link do dataset: https://www.kaggle.com/datasets/loveall/cervical-cancer-risk-classification 
